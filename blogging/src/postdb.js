4e0f358c0febe9603c29cb8829469c15a3cb0f76
renuka <nainalarenukadevi@gmail.com>
2024-12-12 23:39:46 +0530

    added postdb and profiledb

---
+++ blogging/src/postdb.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+async function main() {
+  // mongoose.connect("mongod://localhost:27017/todolistDb", {
+  //   useNewUrlParser: true,
+  // });
+  await mongoose.connect(
+    "mongodb+srv://nainalarenukadevi:TUbZOCuEe1isWhcO@cluster0.olmgv.mongodb.net/",
+    { useNewUrlParser: true }
+  );
+  // mongoose.connect("mongodb://127.0.0.1:27017/myblog") for local DB
+  console.log("post connected");
+}
+main()
+const postSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    content: String,
+    thumbnail:String,
+    date:Number,
+    like:Number,
+    likedby:[String]
+    });
+  
+    
+
+const PosT = mongoose.model("post", postSchema);
+module.exports = PosT
